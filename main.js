ae2a0bd674dba9b21769d69d4f21f89370e50bc1
svyatoslavPy <svyatoslavik9@gmail.com>
2022-09-25 19:53:03 +0300

    created img, mainJs

---
+++ main.js
@@ -0,0 +1,6 @@
+const button = document.querySelector('.element__picture');
+
+button.addEventListener('click', function () {
+    document.querySelector(".info__text").style.display = "flex";
+});
+

8e8031273143d58556c7c672d20aa9465d5666bb
svyatoslavPy <svyatoslavik9@gmail.com>
2022-09-30 14:09:12 +0300

    update style, index, js

--- js/main.js
+++ js/main.js
@@ -4,10 +4,32 @@ const button = document.querySelector('.picture__btn');
 const infoText = document.querySelector('.info');
 // console.log(infoText);
 
+// btn__info
+
+let imgs = document.querySelectorAll('about-us .about-us__title img');
+let next = document.querySelector('.about-us .btn .btn__slider--primary');
+
+let imgPicture = 0;
+
+// const buttonSlider = document.querySelector('.btn__slider');
+// const buttonSlider_primary = document.querySelector('btn__slider--primary');
+// const buttonSlider_secondary = document.querySelector('btn__slider--secondary');
+
+
 button.addEventListener('click', () => {
         infoText.classList.toggle('info_active');
     });
 
+
+next.addEventListener('click', () =>  {
+     imgs[imgPicture].style.opacity = 0;
+    if (imgPicture == imgs.length-1 ) {
+        imgPicture = 0;
+    } else {
+        imgPicture++;
+    }
+    imgs[imgPicture].style.opacity = 1;
+});
 
 
 
